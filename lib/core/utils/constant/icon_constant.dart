7afce7fdb40cd7b4407e1f8b421555dea0a1218d
skkeskar2000 <saurabhkeskar2000@gmail.com>
2024-11-04 14:53:53 +0530

    ai button design completed

--- lib/core/utils/constant/icon_constant.dart
+++ lib/core/utils/constant/icon_constant.dart
@@ -5,4 +5,5 @@ class IconConstant {
   static const overviewTabIcon = '$_basePath/bottom_tab/over_view_tab.svg';
   static const resultTabIcon = '$_basePath/bottom_tab/result_tab.svg';
   static const aiAnalysisTabIcon = '$_basePath/bottom_tab/ai_analysis_tab.svg';
+  static const scanIcon = '$_basePath/scan_icon.svg';
 }
--- lib/features/app/presentation/pages/bottom_tab/overview_page.dart
+++ lib/features/app/presentation/pages/bottom_tab/overview_page.dart
@@ -1,6 +1,8 @@
 import 'package:dental_ui/core/utils/color_utils.dart';
+import 'package:dental_ui/core/utils/constant/icon_constant.dart';
 import 'package:dental_ui/core/utils/responsive_utils/responsive_padding.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OverViewPage extends StatelessWidget {
   const OverViewPage({super.key});
@@ -14,7 +16,7 @@ class OverViewPage extends StatelessWidget {
           SliverAppBar(
             // floating: true,
             // snap: true,
-            expandedHeight: 10,
+            toolbarHeight: 40,
             flexibleSpace: FlexibleSpaceBar(
               background: FilledButton(
                 onPressed: () {},
@@ -23,37 +25,99 @@ class OverViewPage extends StatelessWidget {
             ),
           ),
           SliverAppBar(
-              expandedHeight: 250,
-              floating: true,
-              snap: true,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 20),
-                    Text(
-                      "AI Analysis",
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    const SizedBox(height: 20),
-                    Container(
-                      height: 150,
-                      width: double.maxFinite,
-                      decoration: BoxDecoration(
-                          color:
-                              ColorUtils.get(context).primary.withOpacity(0.8),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Row(
-                        children: [
-                          Column(
-                            children: [],
+            expandedHeight: 210,
+            floating: true,
+            snap: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
+                  Text(
+                    "AI Analysis",
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    height: 120,
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(
+                        color: ColorUtils.get(context).primary.withOpacity(0.8),
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: const EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 8,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Start your AI Analysis",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleLarge!
+                                    .copyWith(
+                                        color: ColorUtils.get(context).surface),
+                              ),
+                              Text(
+                                "Keep Track of your oral health, click here to start.",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyLarge!
+                                    .copyWith(
+                                        color: ColorUtils.get(context).surface),
+                              ),
+                              Text(
+                                "Last Analysis : 02/10/2024",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(
+                                        color: ColorUtils.get(context).surface),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: SvgPicture.asset(
+                            IconConstant.scanIcon,
+                            colorFilter: ColorFilter.mode(
+                                ColorUtils.get(context).surface,
+                                BlendMode.srcIn),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Past Analysis",
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-              )),
+                const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("View More"),
+                    SizedBox(width: 5),
+                    Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      size: 12,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
           SliverList.builder(
             itemCount: 5,
             itemBuilder: (context, index) => Container(
@@ -67,7 +131,7 @@ class OverViewPage extends StatelessWidget {
                 children: [],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
